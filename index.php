9fe24ee26ac04a882558ea3efe74183346b2843a
Rashid <rashid.yaqoob@axelerant.com>
2024-05-08 15:15:42 +0530

    PHP-01: Leaning about concepts

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
+function getStatus($message) {
+  $messageStatus = $message;
+  echo($messageStatus);
+}
 
-echo('hello world');
+getStatus("hello wordsld");
+
+
+// Short circuiting.
+function example() {
+  return "Hello example";
+}
+
+var_dump(true && example());
+
+$a = 1;
+
+while($a < 15) {
+  echo $a . "<br>";
+  $a++;
+}
+
+for($i=0; $i<4; $i++) {
+  echo( 'test for loop ,<br>');
+}
