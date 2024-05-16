915ab7c05174cc64ef348e2847e8b840b76901e7
Rashid <rashid.yaqoob@axelerant.com>
2024-05-16 14:25:11 +0530

    RHDX-5524: Learn about namespaces and classes.

--- index.php
+++ index.php
@@ -2,40 +2,18 @@
 <h1>Home page</h1>
 
 <?php
-require_once 'Account.php';
+require_once 'App/Account.php';
+require_once 'App/SocialMedia.php';
 
-$users = ["John", "Doe", "Ala"];
-
-if (array_key_exists(2, $users)) {
-    echo 'Users found';
-}
-
-$users = array_filter($users, fn($user) => $user !== "Doe");
-
-print_r(array_values($users));
+use App\Account;
 
 
-function getNewName(): string
-{
-    // Generate two random uppercase letters
-    $letters = range(A, Z);
-    shuffle(($letters));
+$dateTime = new DateTime();
 
-    // Generate three random digits
-    $digits = rand(0, 999);
 
-    // Return the combined result
-    return $letters[0] . $letters[4] . $digits;
-}
+$account = new Account('John', 30, 'Social');
 
-// Example of using the function
-echo getNewName() . '<br> <br>';
+var_dump($account);
 
-$account = new Account;
-$myAccount = new Account;
-$account->name = 'Rasj';
-
-var_dump($account->name);
-var_dump($myAccount->name);
 
 
