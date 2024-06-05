c2e0f47453389566d899e2ed7524f7832e8a062e
Rashid <rashid.yaqoob@axelerant.com>
2024-06-05 09:51:27 +0530

    Project setup

--- App/FoodApp.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App;
-
-class FoodApp {
-  public function __construct(RestaurantInterface $restaurant) {
-    $restaurant->prepareFood();
-  }
-}
--- index.php
+++ index.php
@@ -1,55 +1,6 @@
-<?php require './nav.php'; ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="public/css/styles.css">
-<title>My PHP Project</title>
-</head>
-
-<body>
-<h1 class="text-3xl font-bold underline">Home page</h1>
-</body>
-
-</html>
-
 <?php
-spl_autoload_register(function ($class) {
-    $formattedClass = str_replace('\\', '/', $class);
-    $path = "{$formattedClass}.php";
-    include_once $path;
-    var_dump($class);
-});
 
+declare(strict_types=1);
+$app  = include __DIR__ . "/src/App/bootstrap.php";
 
-use App\{Account, Socialmedia, Utility, RestaurantOne, RestaurantTwo, FoodApp};
-
-
-$dateTime = new DateTime();
-echo '<br><br><br>';
-$restaurant = new FoodApp(
-    new RestaurantTwo()
-);
-
-
-$account = new Account('John', 30);
-$socialMedia = new Socialmedia();
-
-$account->deposit(50)->deposit(20);
-
-echo '<br><br><br>';
-
-echo '<br><br><br>';
-
-var_dump($account->getBalance() . ' Get balance');
-echo '<br><br><br>';
-var_dump($account->setBalance(100) . ' Set balance');
-
-var_dump($socialMedia);
-echo '<br><br><br>';
-var_dump($dateTime);
-echo '<br><br><br>';
-Utility::printArr([32,3,3,4,4]);
+$app->run();
